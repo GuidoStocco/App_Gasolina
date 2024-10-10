f668c7a5f1bd9133b793a2e55c41bfa9781567bd
unknown <guidostoccodev@gmail.com>
2024-10-10 19:40:57 -0300

    Adicionando o logo e modificando o estilos

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Modal, Image} from 'react-native';
 
 
 
 export default function App(){
+
+  const img = require('./assets/logo.png')
+
   return(
-    <View>
-      <Text>Guido marinbho</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+          <Image source={img}/>
+      </View>
+    </SafeAreaView>
   );
 };
+
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#121212'
+  }
+})
