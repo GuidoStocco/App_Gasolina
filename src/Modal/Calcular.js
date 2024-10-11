81a9f9ff7e952c9c05335f3f3777cf7b49a127f3
unknown <guidostoccodev@gmail.com>
2024-10-10 21:36:18 -0300

    WKP:criando modal

--- src/Modal/Calcular.js
+++ src/Modal/Calcular.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
 
 export default function Calculo(){
+
+    const [btnVoltar, setBtnVoltar] = useState(false)
+
     return(
         <View>
             <Text>Guido stocco</Text>
+            <Button title='Voltar' onPress={() => {}} />
         </View>
     );
 };
